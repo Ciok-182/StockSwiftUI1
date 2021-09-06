b73ea2b4c822e727ae714f3469135ad5071321d4
Jorge Encinas <jorge.encinas@devant.com.mx>
2021-09-06 12:23:07 -0500

    Initial Commit

---
+++ Stocks1SUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stocks1SUI
+//
+//  Created by Jorge Encinas on 06/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
